0b520a67fc23fea3bfeb6c04e454e830dbcc6071
korostylev-ka <k.korostylev@gmail.com>
2022-05-27 11:10:59 +0500

    armatura class

---
+++ src/armatura/Armatura.java
@@ -0,0 +1,11 @@
+package armatura;
+
+public class Armatura {
+    private int diametr;
+    private double weight;
+    private double lengthOfRod;
+    public Armatura(int diametr, double weight) {
+        this.diametr = diametr;
+        this.weight = weight;
+    }
+}
